e0e97df60e75acb8caa4ab72e6ccae93676c0d9c
조민수 <chominsu10@gmail.com>
2021-08-25 13:23:23 +0900

    [ADD ] chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,27 @@
+const socket = new WebSocket("ws://" + window.location.host);
 
+const messageList = document.querySelector("ul");
+const messageFrom = document.querySelector("form");
+
+socket.addEventListener("open", () => {
+  console.log("서버에 연결됨");
+});
+
+socket.addEventListener("message", messsage => {
+  console.log("서버에서 받은 메세지", messsage.data);
+  const li = document.createElement("li");
+  li.innerHTML = messsage.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("서버와 연결 해제됨");
+});
+
+messageFrom.addEventListener("submit", e => {
+  e.preventDefault();
+  const input = messageFrom.querySelector("#id");
+  const inputName = messageFrom.querySelector("#name");
+  socket.send("이름: " + inputName.value + "내용: " + input.value);
+  input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+import { Http2ServerRequest } from "http2";
 
 const app = express();
 
@@ -6,7 +9,29 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 console.log("http://localhost:3020/");
 
-app.listen(3020);
+// app.listen(3020);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", socket => {
+  sockets.push(socket);
+  console.log("브라우저에 연결됨");
+  socket.on("close", () => {
+    console.log("브라우저와 연결 해제됨");
+  });
+  socket.send("hello!!!");
+
+  socket.on("message", message => {
+    console.log(message.toString("utf8"));
+    sockets.forEach(aSocket => aSocket.send(message.toString("utf8")));
+  });
+});
+
+server.listen(3020);
